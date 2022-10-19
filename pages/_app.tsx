3e7b21126eb6c41e1e4119ab01d6127e6445e3ae
jameszokah <jameszokah@gmail.com>
2022-10-19 15:47:06 +0000

    added @ts-ignore to /pages/_app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,12 @@ import {SessionProvider} from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import {RecoilRoot} from  'recoil'
 
+// @ts-ignore
 function MyApp({ Component, pageProps: {session , ...pageProps} }: AppProps) {
+  
   return <SessionProvider session={session}>
   <RecoilRoot>
+    {/* @ts-ignore */}
     <Component {...pageProps} />
   </RecoilRoot>
   </SessionProvider> 
